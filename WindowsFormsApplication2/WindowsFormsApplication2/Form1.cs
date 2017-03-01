f6cefe241a40d8efc598fcbab6a143ef316d5402
MrBlackDog <sasha29091999@yandex.ru>
2017-03-01 16:18:17 +0300

    Add files via upload

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace WindowsFormsApplication2
 {
@@ -15,6 +16,29 @@ namespace WindowsFormsApplication2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabControl1.TabPages["tabPage2"];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != textBox4.Text)
+                MessageBox.Show("Пароли не совпадают");
+            else
+                tabControl1.SelectedTab = tabControl1.TabPages["tabPage4"];
+        }
+
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = null;
+        }
+
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = null;
         }
     }
 }
